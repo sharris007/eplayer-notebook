95f35f0fc8b9277483fcc8aa1eb59df558c92481
Mani, Iyappan m <Iyappan.Mani@pearson.com>
2017-01-12 15:34:26 +0530

    Initial Commit

---
+++ app/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import RaisedButton from 'material-ui/RaisedButton';
+class App extends React.Component{
+  render(){
+    return (
+      <div id="econtainer">
+        <div>
+          Hello World !!!
+          </div>
+      </div>
+    )
+  }
+}
+export default App;
