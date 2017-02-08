37d1df88f09d3826aebce5462b28e20aff757f6f
Kampalli <429180@CTS.COM>
2017-02-08 15:24:21 +0530

    develop branch

---
+++ pxe-pageviewer/main.js
@@ -0,0 +1,33 @@
+import './main.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PageViewer from './src/js/PageViewer';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+export default class PageViewerComponent {
+  constructor(config) {
+    this.init(config);
+  }
+
+  init=config=>{
+    const App = () => (
+      <MuiThemeProvider>
+         <PageViewer src={config.urlsJson} sendPageDetails={config.sendPageDetails} currentPlayList={config.currentPlayList}/>
+      </MuiThemeProvider>
+     );
+    ReactDOM.render(
+       <App/>, document.getElementById(config.elementId)
+    );
+  };  
+};
+
+export PageViewer from './src/js/PageViewer';
+// Listen for client events to initialize a new PageViewer component
+document.body.addEventListener('o.InitPageViewer', e => new PageViewerComponent(e.detail));
