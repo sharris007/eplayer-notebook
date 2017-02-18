1eeb2458ad22dd65ca234ffc002df6c040fc3583
Aravindan <aravindan.venkatesan@pearson.com>
2017-02-19 02:29:55 +0530

    Updated Annotator file changes and fixed eslint issues

--- annotaion-plugin/Gruntfile.js
+++ annotaion-plugin/Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     uglify: {
         build: {
             files: {
-                'bundle.js': ["src/lib/vendor/json2.js","src/lib/util.js","src/lib/console.js","src/lib/class.js","src/lib/range.js","src/lib/annotator.js","src/lib/widget.js","src/lib/editor.js","src/lib/viewer.js","src/lib/notification.js","src/lib/xpath.js","src/lib/plugin/store.js","src/lib/plugin/permissions.js","src/lib/plugin/annotateitpermissions.js","src/lib/plugin/auth.js","src/lib/plugin/tags.js","src/lib/plugin/unsupported.js","src/lib/plugin/filter.js"]
+                'output/annotator.min.js': ["src/lib/vendor/json2.js","src/lib/util.js","src/lib/console.js","src/lib/class.js","src/lib/range.js","src/lib/annotator.js","src/lib/widget.js","src/lib/editor.js","src/lib/viewer.js","src/lib/notification.js","src/lib/xpath.js","src/lib/plugin/store.js","src/lib/plugin/permissions.js","src/lib/plugin/annotateitpermissions.js","src/lib/plugin/auth.js","src/lib/plugin/tags.js","src/lib/plugin/unsupported.js","src/lib/plugin/filter.js"]
             }
         }
     },
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
       },
       target: {
         files: {
-          'bundle.css':["src/css/annotator.css"]
+          'output/annotator.min.css':["src/css/annotator.css"]
         }
       }
     }
