7b9435723fad7388ac0fd5cf29f548927ef90c6e
saravanakumar.kathirvel <saravanakumar.kathirvel@pearson.com>
2017-03-15 20:06:56 +0530

    Wrapper Component

---
+++ pxe-wrapper-component/const/PopUpClasses.js
@@ -0,0 +1,13 @@
+export const GlossaryPopUpClasses = [
+  'a.keyword',
+  'dfn.keyword',
+  'a.noteref',
+  'dfn.reminder'
+];
+
+export const  MoreInfoPopUpClasses = [
+  '.lc_ec_aside',
+  'a.noteref.noteref_footnote',
+  'a.noteref.noteref_footnote_symboled'
+];
+
