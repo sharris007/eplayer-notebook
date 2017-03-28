3ffe480607d31ea95f8454f260c22d9e92cb44b3
Vamshi <vamshi.kampalli@pearson.com>
2017-03-28 15:06:33 +0530

    removal of events

--- pxe-annotation/src/js/Annotation.js
+++ pxe-annotation/src/js/Annotation.js
@@ -1,6 +1,6 @@
 /* global $ */
 import React, { PropTypes, Component } from 'react';
-import { map,zipObject} from 'lodash';
+import { map, zipObject} from 'lodash';
 
 class Annotation extends Component {
   constructor(props) {
@@ -25,43 +25,48 @@ class Annotation extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.state.firstLoad && nextProps.annotationData.length && nextProps.annotationData[0].color) {
-      console.log("loadAnnotations", nextProps.annotationData);
+    if (this.state.firstLoad && nextProps.annotationData.length && nextProps.annotationData[0].color) {
+      console.log('loadAnnotations', nextProps.annotationData);
       $('#' + nextProps.contentId).annotator().annotator('loadAnnotations', nextProps.annotationData);
       this.setState({'firstLoad':false});
     }
-    if(this.state.updated)  
+    if (this.state.updated) {
       $('#' + nextProps.contentId).annotator().annotator('updateAnnotationId', nextProps.annotationData[0]);
+    }
     this.setState({'updated':false});
   }  
 
   annotationEventHandler() {
     const annotation = $('#' + this.props.contentId).annotator();
-    annotation.data('annotator').on('beforeAnnotationCreated', this.annotationEvent.bind(null, 'beforeAnnotationCreated'));
-    annotation.data('annotator').on('annotationCreated', this.annotationEvent.bind(null, 'annotationCreated'));
-    annotation.data('annotator').on('beforeAnnotationUpdated', this.annotationEvent.bind(null, 'beforeAnnotationUpdated'));
-    annotation.data('annotator').on('annotationUpdated', this.annotationEvent.bind(null, 'annotationUpdated'));
+    // annotation.data('annotator').on('beforeAnnotationCreated', this.annotationEvent.bind(null, 'beforeAnnotationCreated'));
+    // annotation.data('annotator').on('annotationCreated', this.annotationEvent.bind(null, 'annotationCreated'));
+    // annotation.data('annotator').on('beforeAnnotationUpdated', this.annotationEvent.bind(null, 'beforeAnnotationUpdated'));
+    // annotation.data('annotator').on('annotationUpdated', this.annotationEvent.bind(null, 'annotationUpdated'));
     annotation.data('annotator').on('annotationDeleted', this.annotationEvent.bind(null, 'annotationDeleted'));
-    annotation.data('annotator').on('annotationEditorShown', this.annotationEvent.bind(null, 'annotationEditorShown'));
-    annotation.data('annotator').on('annotationEditorHidden', this.annotationEvent.bind(null, 'annotationEditorHidden'));
+    // annotation.data('annotator').on('annotationEditorShown', this.annotationEvent.bind(null, 'annotationEditorShown'));
+    // annotation.data('annotator').on('annotationEditorHidden', this.annotationEvent.bind(null, 'annotationEditorHidden'));
     annotation.data('annotator').on('annotationEditorSubmit', this.annotationEvent.bind(null, 'annotationEditorSubmit'));
-    annotation.data('annotator').on('annotationViewerShown', this.annotationEvent.bind(null, 'annotationViewerShown'));
-    annotation.data('annotator').on('annotationViewerTextField', this.annotationEvent.bind(null, 'annotationViewerTextField'));
+    // annotation.data('annotator').on('annotationViewerShown', this.annotationEvent.bind(null, 'annotationViewerShown'));
+    // annotation.data('annotator').on('annotationViewerTextField', this.annotationEvent.bind(null, 'annotationViewerTextField'));
   }
 
   annotationEvent(eventType, data, viewer) {    
     const customAttributes = this.props.annAttributes;
-    if(data.annotation){
-      const annData             =   data.annotation;
+    if (data.annotation||data) {
+      const annData             =   data.annotation||data;
       annData.createdTimestamp  =   new Date().toISOString();
       annData.updatedTimestamp  =   null;
       const customUnsourceObj   = _.mapKeys(annData, function(value, key) {
-        if(customAttributes[key] ==undefined){
+        if (customAttributes[key] ===undefined) {
           return key;
         }
         return customAttributes[key];
-      }); 
-      if(eventType=='annotationCreated'){
+      });
+      // change of event type on annotationEditorSubmit based on id
+      if (eventType==='annotationEditorSubmit') {
+        eventType=annData.id?'annotationUpdated':'annotationCreated'
+      }  
+      if (eventType==='annotationCreated') {
         this.setState({'updated':true});
       }
       this.props.annotationEventHandler(eventType, customUnsourceObj, viewer);
