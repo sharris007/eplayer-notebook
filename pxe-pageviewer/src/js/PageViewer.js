ec59080f58e50e7e9e5fb24e71b8c34be201b403
Rajkumar <rajkumar.velusamy@pearson.com>
2017-04-25 16:46:30 +0530

    ETWEB-4427-js--font-zoom-in-out

--- pxe-pageviewer/src/js/PageViewer.js
+++ pxe-pageviewer/src/js/PageViewer.js
@@ -273,7 +273,7 @@ class PageViewer extends React.Component {
  
   render() {
     const zommLevel = this.props.src.pageZoom ? this.props.src.pageZoom + '%' : '100%';
-    const fontSize = this.props.src.pageFontSize ? this.props.src.pageFontSize + 'px' : '16px';
+    const fontSize = this.props.src.pageFontSize ? (this.props.src.pageFontSize/3.5) + 'px' : '16px';
     return ( 
       <div id = "book-render-component" ref = {(el) => { this.bookComBlock = el; }} tabIndex = "0" onKeyUp = {this.arrowNavigation} >
         <div id={this.props.src.contentId}>
