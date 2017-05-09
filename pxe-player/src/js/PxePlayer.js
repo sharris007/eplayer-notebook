20b834144a68729fe50329c375e25caa45919863
Vamshi <vamshi.kampalli@pearson.com>
2017-05-09 22:41:22 +0530

    pxe-player-changes-with-integration

--- pxe-player/src/js/PxePlayer.js
+++ pxe-player/src/js/PxePlayer.js
@@ -7,29 +7,32 @@ import { Annotation } from 'pxe-annotation';
 import { Wrapper } from 'pxe-wrapper';
 import { PopUpInfo } from '@pearson-incubator/popup-info';
 
-import {customAttributes} from './constants';
+import { customAttributes, playerConstants } from './constants';
 
 class PxePlayer extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      urlParams:this.props.bootstrapParams.urlParams,
-      currentPageDetails:{},
-      annAttributes:customAttributes, // import from Mocdata
-      popUpCollection:[],
-      annotationData:{}
+    this.state = {
+      urlParams: this.props.bootstrapParams.urlParams,
+      currentPageDetails: {},
+      annAttributes: customAttributes, // import from Mocdata
+      popUpCollection: [],
+      annotationData: {
+        rows: [],
+        total: 0
+      }
     };
   }
   onPageChange = (type, data) => {
     const parameters = this.state.urlParams;
-    parameters.id    = data.id;
-    parameters.uri   = encodeURIComponent(data.href);
-    data.uri         = data.href;
-    data.label       = data.title;
+    parameters.id = data.id;
+    parameters.uri = encodeURIComponent(data.href);
+    data.uri = data.href;
+    data.label = data.title;
     this.setState({
-      currentPageDetails :data,  
-      urlParams:parameters
-    }, function() {
+      currentPageDetails: data,
+      urlParams: parameters
+    }, function () {
       if (this.props.applnCallback) {
       // eslint-disable-next-line
       // this.props.dispatch(getAnnCallService(this.state.urlParams)); // Enable when Annotation component added
@@ -38,31 +41,31 @@ class PxePlayer extends React.Component {
     });
   };
   annotationCallDispatch = (method, data) => {
-    const {pageDetails} = this.props.bootstrapParams;
-    const payload={ // eslint-disable-line no-undef
-      method: method,
+    const { pageDetails } = this.props.bootstrapParams;
+    const payload = { // eslint-disable-line no-undef
+      method,
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Identity-Id':this.state.urlParams.user
+        'Identity-Id': this.state.urlParams.user
       }
     };
     if (data) {
-      payload.body=JSON.stringify(data);
+      payload.body = JSON.stringify(data);
     }
-    //for post url
-    let requestUrl=`${pageDetails.endPoints.services}/context/${this.state.urlParams.context}/annotations`;
+    // for post url
+    let requestUrl = `${pageDetails.endPoints.services}/context/${this.state.urlParams.context}/annotations`;
     switch (method) {
-    case 'GET': {
-      requestUrl=requestUrl+`?uri=${this.state.urlParams.uri}`;
-      break;
-    }
-    case 'PUT': 
-    case 'DELETE':
-      {
-        requestUrl=requestUrl+`/${data.id}`;
+      case 'GET': {
+        requestUrl += `?uri=${this.state.urlParams.uri}`;
         break;
       }
+      case 'PUT':
+      case 'DELETE':
+        {
+          requestUrl += `/${data.id}`;
+          break;
+        }
     }
     return fetch(requestUrl, payload)
       .then((response) => {
@@ -70,85 +73,94 @@ class PxePlayer extends React.Component {
           return response.json();
         }
         throw new Error('Network response was not ok.');
-      }).then((res) => { 
+      }).then(res =>
         // console.log(res);
-        return res;
-      }).catch((error) => {
-        console.log('There has been a problem with your fetch operation: ' + error.message);
-        return false;
-      });
+         res).catch((error) => {
+           console.log(`There has been a problem with your fetch operation: ${error.message}`);
+           return false;
+         });
   };
   onBookLoaded = (isBookLoaded) => {
     if (isBookLoaded) {
-      const that = this;  
-      window.renderPopUp = function(collection) {
-        that.setState({ popUpCollection : collection });
+      const that = this;
+      window.renderPopUp = function (collection) {
+        that.setState({ popUpCollection: collection });
       };
-      this.setState({ popUpCollection : [] });
-      this.wrapper = new Wrapper({'divGlossaryRef' : this.divGlossaryRef, 'bookDiv' : 'book-container'}); // import Wrapper
+      this.setState({ popUpCollection: [] });
+      this.wrapper = new Wrapper({ divGlossaryRef: this.divGlossaryRef, bookDiv: 'book-container' }); // import Wrapper
       this.wrapper.bindPopUpCallBacks();
       // this.getAnnotationData();
-      this.annotationCallDispatch('GET').then((res) => { 
-        const annotationData={...res};
-        this.setState({annotationData:annotationData});
+      this.annotationCallDispatch('GET').then((res) => {
+        const annotationData = { ...res };
+        this.setState({ annotationData });
       });
     }
   };
 
   annotationCallBack = (eventType, data) => {
-    const receivedAnnotationData    = data;
-    receivedAnnotationData.user     = this.state.urlParams.user;
-    receivedAnnotationData.context  = this.state.urlParams.context;
-    receivedAnnotationData.source   = this.state.currentPageDetails;
+    const receivedAnnotationData = data;
+    receivedAnnotationData.user = this.state.urlParams.user;
+    receivedAnnotationData.context = this.state.urlParams.context;
+    receivedAnnotationData.source = this.state.currentPageDetails;
     receivedAnnotationData.source.baseUrl = this.props.bootstrapParams.pageDetails.baseUrl;
       // delete receivedAnnotationData.source.href;
       // delete receivedAnnotationData.source.title;
     switch (eventType) {
-    case 'annotationCreated': {
+      case 'annotationCreated': {
       // this.createAnnotation(receivedAnnotationData).then((newAnnotation)=>{
       //   const annotationData={...this.state.annotationData};
       //   annotationData.total=annotationData.total+1;
       //   annotationData.rows.push(newAnnotation);
       //   this.setState({annotationData:annotationData});
       // });
-      this.annotationCallDispatch('POST', receivedAnnotationData).then((newAnnotation)=>{
-        const annotationData={...this.state.annotationData};
-        annotationData.total=annotationData.total+1;
-        annotationData.rows.push(newAnnotation);
-        this.setState({annotationData:annotationData});
-      });
-      break;
-    }
-    case 'annotationUpdated': {
+        this.annotationCallDispatch('POST', receivedAnnotationData).then((newAnnotation) => {
+          const annotationData = { ...this.state.annotationData };
+          annotationData.total += 1;
+          annotationData.rows.push(newAnnotation);
+          this.setState({ annotationData }, () => {
+            // Making empty so that Annotation component loads with new annotation created and duplicates the existing one.
+            this.setState({ annotationData: [] });
+          });
+          this.props.applnCallback(playerConstants.ANNOTATION_CREATED, newAnnotation);
+        });
+        break;
+      }
+      case 'annotationUpdated': {
       // this.updateAnnotation(receivedAnnotationData);
-      this.annotationCallDispatch('PUT', receivedAnnotationData);
-      break;
-    }
-    case 'annotationDeleted': {
+        this.annotationCallDispatch('PUT', receivedAnnotationData).then((updatedAnnotation) => {
+          this.props.applnCallback(playerConstants.ANNOTATION_UPDATED, updatedAnnotation);
+        });
+        break;
+      }
+      case 'annotationDeleted': {
       // receivedAnnotationData.annId    = data.id;
       // this.deleteAnnotation(receivedAnnotationData);
-      this.annotationCallDispatch('DELETE', receivedAnnotationData);
-      break;
-    }
-    default : {
-      return eventType;
-    }
+        this.annotationCallDispatch('DELETE', receivedAnnotationData).then((deletedAnnotation) => {
+          this.props.applnCallback(playerConstants.ANNOTATION_DELETED, deletedAnnotation);
+        });
+        break;
+      }
+      default : {
+        return eventType;
+      }
     }
   }
   render() {
-    const {bootstrapParams} = this.props;
-    const {annotationData}=this.state;
-    return(<div>
-            <PageViewer src={bootstrapParams.pageDetails} sendPageDetails={this.onPageChange} onBookLoaded = {(bload) => this.onBookLoaded(bload)} applnCallback={this.props.applnCallback}/>    
-            {this.state.popUpCollection.length > 0 ? <PopUpInfo popUpCollection = {this.state.popUpCollection} bookId = "book-container"/> : ''}
-            <div id= "divGlossary" ref = {(dom) => { this.divGlossaryRef = dom; }} style = {{ display: 'none' }}>  </div>
-            <Annotation annAttributes = {this.state.annAttributes} shareableAnnotations={bootstrapParams.pageDetails.annotationShareable} annotationData={annotationData.rows} contentId="pxe-viewer"
-            annotationEventHandler={this.annotationCallBack} />
-          </div>);
+    const { bootstrapParams } = this.props;
+    const { annotationData } = this.state;
+    return (<div>
+      <PageViewer src={bootstrapParams.pageDetails} sendPageDetails={this.onPageChange} onBookLoaded={bload => this.onBookLoaded(bload)} applnCallback={this.props.applnCallback} />
+      {this.state.popUpCollection.length > 0 ? <PopUpInfo popUpCollection={this.state.popUpCollection} bookId="book-container" /> : ''}
+      <div id="divGlossary" ref={(dom) => { this.divGlossaryRef = dom; }} style={{ display: 'none' }} />
+      <Annotation
+        annAttributes={this.state.annAttributes} shareableAnnotations={bootstrapParams.pageDetails.annotationShareable} annotationData={annotationData.rows} contentId="pxe-viewer"
+        annotationEventHandler={this.annotationCallBack}
+            />
+    </div>);
   }
-};
+}
 
 PxePlayer.PropTypes = {
-  bootstrapParams:PropTypes.object.isRequired
+  bootstrapParams: PropTypes.object.isRequired
 };
 export default PxePlayer;
--- pxe-player/src/js/constants.js
+++ pxe-player/src/js/constants.js
@@ -1,6 +1,6 @@
 export const apiConstants = {
   PXESERVICE: 'https://pxe-sdk-qa.stg-openclass.com/services-api/api/3.1',
-  PAPERBASE :'https://paperapi-qa.stg-openclass.com/nextext-api/api/nextext',
+  PAPERBASE: 'https://paperapi-qa.stg-openclass.com/nextext-api/api/nextext',
  // PAPERBASE :'https://etext-stg.pearson.com/api/nextext-api/api/nextext',
   BOOK: 'https://api.mongolab.com/api/1/databases/team4/collections/students',
   APIKEY: 'AgffBG36UOuFvRNORM8Y1ACvA1FdJwN8',
@@ -10,14 +10,19 @@ export const apiConstants = {
 
 export const customAttributes = {
   playOrder: 'playOrder',
-  href     :'href',
-  createdTimestamp:'createdTimestamp',
-  updatedTimestamp:'updatedTimestamp',
-  text  :'text',
-  user:'user',
-  context:'context',
-  ranges :'ranges',
-  quote:'quote',
-  shareable:'shareable'
+  href: 'href',
+  createdTimestamp: 'createdTimestamp',
+  updatedTimestamp: 'updatedTimestamp',
+  text: 'text',
+  user: 'user',
+  context: 'context',
+  ranges: 'ranges',
+  quote: 'quote',
+  shareable: 'shareable'
+};
+export const playerConstants = {
+  ANNOTATION_CREATED: 'ANNOTATION_CREATED',
+  ANNOTATION_UPDATED: 'ANNOTATION_UPDATED',
+  ANNOTATION_DELETED: 'ANNOTATION_DELETED'
 };
 
