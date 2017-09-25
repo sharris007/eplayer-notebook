11d25ba179b888d4d59f962febae7455b57b3520
saravanakumar.kathirvel <saravanakumar.kathirvel@pearson.com>
2017-09-25 19:52:05 +0530

    On-clicking-annotated-glossary-term-both-popup-appears

--- pxe-annotation/demo/ann-plugin/output/instructor-annotator/instructor-annotator.js
+++ pxe-annotation/demo/ann-plugin/output/instructor-annotator/instructor-annotator.js
@@ -3112,8 +3112,11 @@ Annotator = (function(_super) {
     }
     else
       height = location.top+39;
-    var selctionOverlap = window.getSelection().getRangeAt(0);
-    var iscolorPanel = $(selctionOverlap.startContainer).hasClass('annotator-color-container');
+    var selctionOverlap = '';
+    if(window.getSelection().rangeCount > 0) {
+      selctionOverlap = window.getSelection().getRangeAt(0);
+    }
+    var iscolorPanel = selctionOverlap ? $(selctionOverlap.startContainer).hasClass('annotator-color-container') : '';
     if (iscolorPanel && isAdderClick == false && $('.annotator-editor .annotator-panel-2 .annotator-listing').css('display') == 'none')
       isAdderClick = true;
     var position= {
