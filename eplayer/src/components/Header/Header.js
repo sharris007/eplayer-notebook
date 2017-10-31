202a6ef2941f7a19b4d364dd283a99eb707089ed
Pravinkumar Loganathan <praveenkumar.loganathan@pearson.com>
2017-10-31 23:12:10 +0530

    Header alignment changes

--- eplayer/src/components/Header/Header.js
+++ eplayer/src/components/Header/Header.js
@@ -268,6 +268,27 @@ export class Header extends React.Component {
         navUp: {
           top: '-84px'
         }
+      },
+      hideBookshelfIcon: {
+        margin: '0',
+        visibility:'hidden'
+      },
+      hideDrawerIcon: {
+        margin: '0 0 0 30px',
+        height: '16.5px',
+        width: '18px',
+        visibility:'hidden'
+      },
+      hideBookmarkIcon: {
+        visibility:'hidden'
+      },
+      hidePrefIcon: {
+        margin: '0 0 0 30px',
+        visibility:'hidden'
+      },
+      hideSearchIcon: {
+        margin: '0 30px',
+        visibility:'hidden'
       }
     };
 
@@ -296,7 +317,9 @@ export class Header extends React.Component {
             <div>
               <div className="back_rec">
               {this.props.isET1 === 'Y' && (!this.props.bookData.bookFeatures.hasbookshelflink || this.props.invoketype === 'et1') ?
-                <div className="empty"/> :
+                <div 
+                  className="icon-white "
+                  style={style.hideBookshelfIcon}/> :
                 <span
                   className="icon-white "
                   style={style.bookshelfIcon}
@@ -310,7 +333,9 @@ export class Header extends React.Component {
               }
               </div>
               {this.props.isET1 === 'Y' && !this.props.bookData.bookFeatures.hasdrawerbutton ?
-                <div className="empty"/> :
+                <div 
+                  className="drawerIcon icon-white"
+                  style={style.hideDrawerIcon}/> :
               <span
                 className="drawerIcon icon-white"
                 style={style.drawerIcon}
@@ -327,13 +352,15 @@ export class Header extends React.Component {
           iconElementRight={
             <div>
             {this.props.currentPageIndex == 0 || (this.props.isET1 === 'Y' && !this.props.bookData.bookFeatures.hasbookmarkpagebutton) ?
-              <div className="empty"/> :
+              <div className="bookmarkIcon" style={style.hideBookmarkIcon}/> :
               <div className="bookmarkIcon" onClick={this.handleHeaderClick} role="button" tabIndex="0">
                 <BookmarkIconComponent data={bookmarkIconData} locale="en"/>
               </div>
             }
             {this.props.isET1 === 'Y' && !this.props.bookData.bookFeatures.haszoominbutton && !this.props.bookData.bookFeatures.haszoomoutbutton ?
-              <div className="empty"/> :
+              <div 
+                className="icon-white prefIcon"
+                style={style.hidePrefIcon}/> :
               <div
                 className="icon-white prefIcon"
                 style={style.prefIcon}
@@ -349,7 +376,9 @@ export class Header extends React.Component {
        : <div className="preferences-container">{this.state.prefOpen ? <div className="content"><PreferencesComponent fetch={this.props.getPreference} preferenceUpdate={this.props.updatePreference}
          disableBackgroundColor={false} locale="en" /></div> : <div className="empty" />} </div>}
          {this.props.isET1 === 'Y' && !this.props.bookData.bookFeatures.hassearchbutton ?
-              <div className="empty"/> :
+              <div 
+                className="icon-white searchIcon"
+                style={style.hideSearchIcon}/> :
               <div
                 className="icon-white searchIcon"
                 style={style.searchIcon}
