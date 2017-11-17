86ad1ba81eeee5cb652d466f6a9a59c12bec5b88
Rohit Srivastava <srivastavaro@hcl.com>
2017-11-17 14:48:04 +0530

    eT1 fix : Faceless Audio Not working in Mac

--- eplayer/src/routes/PdfBook/components/PdfBookReader.js
+++ eplayer/src/routes/PdfBook/components/PdfBookReader.js
@@ -207,8 +207,15 @@ export class PdfBookReader extends Component {
           for(let arr=0;arr < this.props.data.book.regions.length ; arr++)
           {
             if(this.props.data.book.regions[arr].regionTypeID == 5 && this.props.data.book.regions[arr].glossaryEntryID !== null)
-            {       
-              glossaryEntryIDsToFetch = glossaryEntryIDsToFetch + "," + this.props.data.book.regions[arr].glossaryEntryID;
+            { 
+              if(glossaryEntryIDsToFetch == '')
+              {
+                glossaryEntryIDsToFetch = glossaryEntryIDsToFetch + this.props.data.book.regions[arr].glossaryEntryID;
+              }
+              else
+              {
+                glossaryEntryIDsToFetch = glossaryEntryIDsToFetch + "," + this.props.data.book.regions[arr].glossaryEntryID;
+              }      
               regionsData.push(this.props.data.book.regions[arr]);
             }
           }
@@ -841,25 +848,25 @@ handleRegionClick(hotspotID) {
                 catch(e){
                 }
               }     
-              else if(this.state.regionData.hotspotType == 'AUDIO' && this.state.regionData.linkTypeID == eT1Contants.LinkType.FACELESSAUDIO)
+              else if(this.state.regionData.hotspotType == 'AUDIO')
               {
-                try
+                jQuery(function(){
+                 jQuery('.play-pause').click();
+                });
+                if(this.state.regionData.linkTypeID == eT1Contants.LinkType.FACELESSAUDIO)
                 {
-                 $('.aquila-audio-player').hide();
-                  jQuery(function(){
-                   jQuery('.play-pause').click();
-                  });
-                }
-                catch(e){
+                  try
+                  {
+                   $('.aquila-audio-player').hide();
+                  }
+                  catch(e){
+                  }                  
                 }
               }
               if(this.state.regionData.hotspotType == 'AUDIO')
               {
                 try
                 {
-                  jQuery(function(){
-                   jQuery('.play-pause').click();
-                  });
                   $.getScript("https://code.jquery.com/ui/1.12.1/jquery-ui.js", function() 
                   {
                     $( ".aquila-audio-player" ).draggable()
