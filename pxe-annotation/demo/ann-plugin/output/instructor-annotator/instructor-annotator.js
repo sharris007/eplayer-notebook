9fbc531c4425cb2a9f9bd6fa398abf760fffefdb
Rajkumar velusamy <rajkumar.velusamy@pearson.com>
2017-12-04 15:51:33 +0530

    resolving merge issues

--- pxe-annotation/demo/ann-plugin/output/instructor-annotator/instructor-annotator.js
+++ pxe-annotation/demo/ann-plugin/output/instructor-annotator/instructor-annotator.js
@@ -2938,7 +2938,7 @@ Annotator = (function(_super) {
 
   Annotator.prototype.updateAnnotationId = function (annotation) {
      $('.annotator-hl').each(function() {
-      if(Date.parse($(this).data("annotation").createdTimestamp) == annotation.createdTimestamp) {
+      if($(this).data("annotation").createdTimestamp.toString() == annotation.createdTimestamp) {
         $(this).data("annotation").id=annotation.id;
         $(this).attr('data-ann-id', annotation.id);
       }
@@ -3277,12 +3277,12 @@ Annotator = (function(_super) {
     var annotations = $(event.target).parents('.annotator-hl').addBack().map(function() {
       return $(this).data("annotation");
     }).toArray();
-    for (_i = 0; _i <annotations.length ; _i++) {
+    /*for (_i = 0; _i <annotations.length ; _i++) {
       if($(annotations)[_i].shareable)
         currAnnPosition = _i;
        if(!($(annotations)[_i].id))
         currAnnPosition++;
-    };
+    };*/
     this.showEditor(annotations[currAnnPosition], Util.mousePosition(event, this.wrapper[0]), false);
  
   }
