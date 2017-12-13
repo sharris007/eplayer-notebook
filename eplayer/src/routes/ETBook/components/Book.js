b819ccc00882986e8399128ea827d378b2ad371d
Saravanakumar <saravanakumar.kathirvel@pearson.com>
2017-12-13 15:45:05 +0530

    GTM-for-search-toc

--- eplayer/src/routes/ETBook/components/Book.js
+++ eplayer/src/routes/ETBook/components/Book.js
@@ -307,8 +307,8 @@ export class Book extends Component {
       'eventAction': 'Removing Notes',
       'event': 'annotationDelete',
       'eventCategory': 'Notes',
-      'text': deletedAnnotationData.text,
-      'selectedText': deletedAnnotationData.comment,
+      'selectedText': deletedAnnotationData.text,
+      'text': deletedAnnotationData.comment,
       'chapterTitle': chapterInfo.title,
       'sectionTitle': sectionInfo.title
     }
@@ -974,6 +974,11 @@ export class Book extends Component {
       }
     });
     this.goToPageCallback(bookObj.id, '', [searchInfo.split('*')[1]]);
+    let obj = {};
+    obj.event = "searchResultClicked";
+    obj.term = searchInfo.split('*')[1];
+    obj.target = this.state.pageDetails.baseUrl +'OPS'+ searchHref;
+    dataLayer.push(obj);
   }
 
   render() {
@@ -1080,11 +1085,19 @@ export class Book extends Component {
               this.setState({
                 idc:false
               });
+              let obj = {};
+              obj.event = "tocPublishSuccess";
+              obj.term = 'TOC published sucessfully';
+              dataLayer.push(obj);
             }
             else {
               this.type = 'Error';
               this.message = "Your changes didn't get published. Give us a few moments and try again.";
-              this.setState({publishedToc:true})
+              this.setState({publishedToc:true});
+              let obj = {};
+              obj.event = "tocPublishError";
+              obj.term = 'Error in TOC publish';
+              dataLayer.push(obj);
             }
             
           }
