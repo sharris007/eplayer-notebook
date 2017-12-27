9ea8d47641141fd9db223193fb347d54d8811a60
Sarathkumar V <sarathkumar.venkatachalam@pearson.com>
2017-12-27 18:37:23 +0530

    noteref_rearnote_

--- pxe-wrapper/main.js
+++ pxe-wrapper/main.js
@@ -29,7 +29,7 @@ export default class PxeWrapperComponent {
     const locale = config.locale ? config.locale : 'en';
     ReactDOM.render(
       <IntlProvider locale={locale} messages={translations[locale]}>
-        <ComponentOwner bookUrl = "https://content.stg-openclass.com/eps/pearson-reader/api/item/e460baf9-c83c-4519-b9ff-1de4a387d109/1/file/donatelle_ketchum-ath-15e_v5_SPI_062017/OPS/xhtml/ch10_pg0003.xhtml" isFromComponent = {true}  />
+        <ComponentOwner bookUrl = "https://content.stg-openclass.com/eps/pearson-reader/api/item/78e0830a-c187-4a9b-b1a7-f728e14f101f/1/file/Blake3-082715-MJ-CM-CSS/OPS/s9ml/chapter01/filep7000496622000000000000000000eb8.xhtml" isFromComponent = {true}  />
       </IntlProvider>,
         document.getElementById(config.contentId)
     );
@@ -46,4 +46,6 @@ https://content.stg-openclass.com/eps/pearson-reader/api/item/c9cb03c3-d1a4-441e
 https://content.stg-openclass.com/eps/pearson-reader/api/item/e460baf9-c83c-4519-b9ff-1de4a387d109/1/file/donatelle_ketchum-ath-15e_v5_SPI_062017/OPS/xhtml/ch10_pg0003.xhtml ------------- rear note
 
 https://content.stg-openclass.com/eps/pearson-reader/api/item/3e6ea386-cd63-43eb-bfa9-de3c2f4e13e8/100/file/Bledsoe_Vol1/OPS/s9ml/chapter01/filep70010122960000000000000000006fc.xhtml ---------- bibloref
+
+https://content.stg-openclass.com/eps/pearson-reader/api/item/78e0830a-c187-4a9b-b1a7-f728e14f101f/1/file/Blake3-082715-MJ-CM-CSS/OPS/s9ml/chapter01/filep7000496622000000000000000000eb8.xhtml
 */
