8da4a8243c6f8df45049ee2d339c3bf837e9de80
Sarathkumar V <sarathkumar.venkatachalam@pearson.com>
2018-01-10 16:16:28 +0530

    biblioref Implementation

--- pxe-wrapper/main.js
+++ pxe-wrapper/main.js
@@ -45,9 +45,12 @@ https://content.stg-openclass.com/eps/pearson-reader/api/item/c9cb03c3-d1a4-441e
 
 https://content.stg-openclass.com/eps/pearson-reader/api/item/e460baf9-c83c-4519-b9ff-1de4a387d109/1/file/donatelle_ketchum-ath-15e_v5_SPI_062017/OPS/xhtml/ch10_pg0003.xhtml ------------- rear note
 
-https://content.stg-openclass.com/eps/pearson-reader/api/item/3e6ea386-cd63-43eb-bfa9-de3c2f4e13e8/100/file/Bledsoe_Vol1/OPS/s9ml/chapter01/filep70010122960000000000000000006fc.xhtml ---------- bibloref
+https://content.stg-openclass.com/eps/pearson-reader/api/item/3e6ea386-cd63-43eb-bfa9-de3c2f4e13e8/100/file/Bledsoe_Vol1/OPS/s9ml/chapter01/filep70010122960000000000000000006fc.xhtml ---------- noteref_bibloref
 
 https://content.stg-openclass.com/eps/pearson-reader/api/item/78e0830a-c187-4a9b-b1a7-f728e14f101f/1/file/Blake3-082715-MJ-CM-CSS/OPS/s9ml/chapter01/filep7000496622000000000000000000eb8.xhtml
 
 https://etext.pearson.com/eps/pearson-reader/api/item/41a45147-8f4e-4420-b4d3-961441119580/1/file/HessPGALA12-060216-MJ-DW/OPS/s9ml/imported_files01/filep7001011641000000000000000002c30.xhtml ------------ noteref_footnote
+
+https://content.stg-openclass.com/eps/pearson-reader/api/item/591fb53c-a53a-47d8-b32e-f2b850403061/1/file/nay4_5-25a_post/OPS/s9ml/chapter01/filep7000499443000000000000000000c87.xhtml  ------------------- bibloref
+
 */
