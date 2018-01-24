4b77fe0262f71adb4e0d0b58a1e9da75675a3172
Kampalli, Vamshi <vamshi.kampalli@pearson.com>
2018-01-24 16:58:32 +0530

    pxescripts_webpack_change

--- eplayer/build/webpack.config.js
+++ eplayer/build/webpack.config.js
@@ -86,7 +86,7 @@ webpackConfig.plugins = [
       { from: path.join(__dirname, '../hotspot_icons'), to: 'hotspot_icons' },
       { from: annotationLibPath, to: 'annotation-lib' },
       { from: webInfPath, to: 'WEB-INF' },
-      { from: path.join(__dirname, '../node_modules/@pearson-incubator/vega-viewer/public/pxe'), to: 'pxe_scripts' },
+      { from: path.join(__dirname, '../public/pxe'), to: 'pxe_scripts' },
       { from: path.join(__dirname, '../node_modules/@pearson-incubator/vega-viewer/demo/assets/js'), to: 'bxix_scripts' }/* ,
       { from: path.join(__dirname, '../css'), to: 'css'}*/
   ]),
