147e2dd24132f39a9d1b512ba8ffa8051cd2de3d
Sarathkumar V <sarathkumar.venkatachalam@pearson.com>
2018-02-12 17:10:46 +0530

    Disable Ise redirection

--- eplayer/const/Settings.js
+++ eplayer/const/Settings.js
@@ -19,7 +19,7 @@ export const resources = {
     PiTokenRefreshTimeout:1800,
     secureApi      : true,
     idcDashboardEnabled : true,
-    iseEnabled : true,
+    iseEnabled : false,
     zeppelinEnabled     : true,
     isDisableAnnotation : false
   },
