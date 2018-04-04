1d7d90393996c197ad326477b3031379ecb427f0
Nithya <nithya.natarajan@pearson.com>
2018-04-04 18:05:57 +0530

    created route for ISE dashboard

---
+++ eplayer/src/routes/ISEdashboard/index.js
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
+ *   
+ *  *  Copyright © 2017 Pearson Education, Inc.
+ *  *  All Rights Reserved.
+ *  * 
+ *  * NOTICE:  All information contained herein is, and remains
+ *  * the property of Pearson Education, Inc.  The intellectual and technical concepts contained
+ *  * herein are proprietary to Pearson Education, Inc. and may be covered by U.S. and Foreign Patents,
+ *  * patent applications, and are protected by trade secret or copyright law.
+ *  * Dissemination of this information, reproduction of this material, and copying or distribution of this software 
+ *  * is strictly forbidden unless prior written permission is obtained from Pearson Education, Inc.
+ *******************************************************************************/
+// import { injectReducer } from '../../store/reducers';
+
+export default () => ({
+  path: '/eplayer/ISEdashboard',
+  /*  Async getComponent is only invoked when route matches   */
+  getComponent(nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+        and embed an async module loader (jsonp) when bundling   */
+    require.ensure([], (require) => {
+      /*  Webpack - use require callback to define
+          dependencies for bundling   */
+      const Print = require('./components/ISEdashboard').default;
+      /*  Return getComponent   */
+      cb(null, Print);
+
+    /* Webpack named bundle   */
+    }, 'isedashboard');
+  }
+});
