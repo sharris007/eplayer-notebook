58240c5c35f719083bad70f7ec85ef8fe7f0303f
Senthilkumar Rajendran <r.senthilkumar@outlook.com>
2018-04-20 20:41:21 +0530

    added notebook

--- eplayer/src/routes/Dashboard/components/dashboard.js
+++ eplayer/src/routes/Dashboard/components/dashboard.js
@@ -235,7 +235,7 @@ export class Dashboard extends Component {
           cardFooter={true}
           tocData={this.tocCompData}
           showCourse={false}
-        />  : <div>Notebook</div>
+        />  : <NoteBookComponenent/>
         }
           
         </div> : null
